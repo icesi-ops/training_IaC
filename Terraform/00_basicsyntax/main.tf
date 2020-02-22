c8631a3b433d64aa290bf1e3cbe4de7d298ab761
icesi-ops <59986692+icesi-ops@users.noreply.github.com>
2020-02-21 23:19:56 -0500

    Update main.tf

--- Terraform/00_basicsyntax/main.tf
+++ Terraform/00_basicsyntax/main.tf
@@ -6,13 +6,19 @@ provider "docker" {
 
 # Create a container
 
-resource "docker_container" "frontend"{
-
-    image = "${docker_image.img_frontend.latest}"
-    name = "CF1"
+resource "docker_container" "frontend" {
+  image = "${docker_image.img_frontend.latest}"
+  name  = "CF1"
+  rm = true
+  attach = false
+  ports = {
+      internal = "80"
+      external = "8080"
+  }
 }
 
-resource "docker_image" "img_frontend"{
-    name = "frontend:latest"
+resource "docker_image" "img_frontend" {
+  name = "facebook:1.0.7"
 }
+
 ```
